5f142939235f406ff448e90dd2d3ddf2a7a2f6fa
BocilAlok <88170940+BocilAlok@users.noreply.github.com>
2022-03-01 18:56:16 +0700

    Add files via upload

---
+++ y.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+print("Masukan Namamu")
+elif raw_input(Nama= %s)
+print("Nama Kamu Adalah= %s")
+print("Benar Bukan?")
+elif raw_input(Ya/Tidak)
+print ("Terimakasih Telah Menggunakan Sc ini") exit()
