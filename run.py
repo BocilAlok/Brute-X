ae7b0c28fbca2a40dbeb1b424fd55446f44cf8f5
BocilAlok <88170940+BocilAlok@users.noreply.github.com>
2022-03-01 18:18:14 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("id").moch_yayan()
+       __import__("Brute-X").moch.sanz()
    except Exception as e:
        exit(str(e))
