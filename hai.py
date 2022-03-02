34da4677e25021de60573720a7752788009203d9
BocilAlok <88170940+BocilAlok@users.noreply.github.com>
2022-03-02 08:49:32 +0700

    Add files via upload

---
+++ hai.py
@@ -0,0 +1,20 @@
+os.system('clear')
+print ("Hallo Tod Login Dulu Ya")
+user = raw_input("UserName : ")
+passw = getpass.getpass('prompt=Password = ')
+
+password = ('zafi')
+
+if passw in password:
+    print ("Selamat Datang " + user + ",Anda Berhasil Login")
+def menu()
+print (1.Masuk)
+print (2.Log Out)
+elif in ('1','01'):
+  print ("Masukan Namamu")
+sanz = raw_input("Nama : ")
+  print (Hallo " + sanz + " )
+os.system('rm -rf hai.py')
+elif in ('2','02'):
+  print ("goodbye")
+os.system('rm -rf hai.py')
